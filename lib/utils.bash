69e50d5caa4e91eb38e211eb2e97717d0044f19e
Arunmozhi <arun@arunmozhi.in>
2024-09-04 22:21:12 +1000

    fix: use the correct download paths for getting tutor binary

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for tutor.
 GH_REPO="https://github.com/overhangio/tutor"
 TOOL_NAME="tutor"
 TOOL_TEST="tutor --version"
@@ -14,7 +13,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if tutor is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -31,8 +29,7 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if tutor has other means of determining installable versions.
+	# By default we simply list the tag names from GitHub releases.
 	list_github_tags
 }
 
@@ -42,10 +39,11 @@ download_release() {
 	filename="$2"
 
 	# TODO: Adapt the release URL convention for tutor
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	# Example URL: https://github.com/overhangio/tutor/releases/download/v18.1.3/tutor-Linux_x86_64
+	url="$GH_REPO/releases/download/$version/$TOOL_NAME-$(uname -s)_$(uname -m)"
 
 	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url. $TOOL_NAME binaries are available only for Linux x64 and Mac OS x64."
 }
 
 install_version() {
